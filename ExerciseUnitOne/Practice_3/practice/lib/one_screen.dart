b022f3868eca9e4d3a09c4aaed6551dd2c2251c8
paolomococci <paolomococci@gmail.com>
2023-01-25 22:17:57 +0100

    new file:   ExerciseUnitOne/Practice_3/practice/lib/one_screen.dart

---
+++ ExerciseUnitOne/Practice_3/practice/lib/one_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class OneScreen extends StatelessWidget {
+
+  const OneScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
