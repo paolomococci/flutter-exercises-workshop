eab3616dd4afaeb6789bbe26bf59ae197c28660b
paolomococci <paolomococci@gmail.com>
2023-01-25 23:33:18 +0100

    modified:   ExerciseUnitOne/Practice_3/practice/lib/one_screen.dart

--- ExerciseUnitOne/Practice_3/practice/lib/one_screen.dart
+++ ExerciseUnitOne/Practice_3/practice/lib/one_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:practice/immutable_widget.dart';
 
 class OneScreen extends StatelessWidget {
 
   const OneScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => Container();
+  Widget build(BuildContext context) => const Scaffold(
+    body: Center(
+      child: AspectRatio(
+        aspectRatio: 1.0,
+        child: ImmutableWidget(),
+      ),
+    ),
+  );
 }
