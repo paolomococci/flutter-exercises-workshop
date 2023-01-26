62139cf694380874a4cef2ab276f98419b7c68b5
paolomococci <paolomococci@gmail.com>
2023-01-26 07:32:41 +0100

    modified:   ExerciseUnitOne/Practice_4/practice/lib/immutable_widget.dart

--- ExerciseUnitOne/Practice_4/practice/lib/immutable_widget.dart
+++ ExerciseUnitOne/Practice_4/practice/lib/immutable_widget.dart
@@ -1,40 +1,57 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as Math;
+import "dart:math" show pi;
+//import "dart:math" as math;
 
 class ImmutableWidget extends StatelessWidget {
-  
   const ImmutableWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.green,
-      child: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Container(
-          color: Colors.purple,
-          child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: _buildShinyCircle(),
-          ),
+      decoration: const BoxDecoration(color: Colors.green),
+      foregroundDecoration: const BoxDecoration(
+          backgroundBlendMode: BlendMode.colorBurn,
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xAA0d6123),
+                Color(0x00000000),
+                Color(0xAA0d6123)
+              ])),
+      child: Center(
+        child: Transform.rotate(
+          angle: 180 / pi,
+          //angle: 180 / math.pi,
+          child: Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                  color: Colors.purple,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.deepPurple.withAlpha(120),
+                        offset: Offset.fromDirection(1.0, 30.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 4.0,
+                        blurStyle: BlurStyle.normal),
+                  ],
+                  borderRadius: const BorderRadius.all(Radius.circular(20))),
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: _buildShinyCircle(),
+              )),
         ),
       ),
     );
   }
-  
+
   Widget _buildShinyCircle() => Container(
-    decoration: const BoxDecoration(
-      shape: BoxShape.circle,
-      gradient: RadialGradient(
-        colors: [
-          Colors.lightBlueAccent,
-          Colors.lightBlue
-        ],
-        center: Alignment(-0.3, -0.5)
-      ),
-      boxShadow: [
-        BoxShadow(blurRadius: 20.0)
-      ]
-    ),
-  );
+        decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: RadialGradient(
+                colors: [Colors.lightBlueAccent, Colors.lightBlue],
+                center: Alignment(-0.3, -0.5)),
+            boxShadow: [BoxShadow(blurRadius: 20.0)]),
+      );
 }
