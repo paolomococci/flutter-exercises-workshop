5c1ca1fe748740e09a5cb84afbb73974b1c6b71f
paolomococci <paolomococci@gmail.com>
2023-01-26 08:13:44 +0100

    modified:   ExerciseUnitOne/Practice_4/practice/lib/main.dart

--- ExerciseUnitOne/Practice_4/practice/lib/main.dart
+++ ExerciseUnitOne/Practice_4/practice/lib/main.dart
@@ -7,7 +7,28 @@ class StaticApp extends StatelessWidget {
   const StaticApp({super.key});
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(
-        home: OneScreen(),
+  Widget build(BuildContext context) => MaterialApp(
+        theme: ThemeData(
+            brightness: Brightness.dark,
+            primaryColor: Colors.green,
+            appBarTheme: AppBarTheme(
+                elevation: 10.0,
+                toolbarTextStyle: const TextTheme(
+                        titleMedium:
+                            TextStyle(
+                              fontFamily: 'Roboto', 
+                              fontWeight: FontWeight.normal, 
+                              fontSize: 24
+                            ))
+                    .bodyText2,
+                titleTextStyle: const TextTheme(
+                        titleMedium:
+                            TextStyle(
+                              fontFamily: 'Roboto', 
+                              fontWeight: FontWeight.normal, 
+                              fontSize: 24
+                            ))
+                    .headline6)),
+        home: const OneScreen(),
       );
 }
