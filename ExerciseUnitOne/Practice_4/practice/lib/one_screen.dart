8e5dbe49fad9ab501bb82e91ac358bab1e9d0f70
paolomococci <paolomococci@gmail.com>
2023-01-26 07:32:58 +0100

    modified:   ExerciseUnitOne/Practice_4/practice/lib/one_screen.dart

--- ExerciseUnitOne/Practice_4/practice/lib/one_screen.dart
+++ ExerciseUnitOne/Practice_4/practice/lib/one_screen.dart
@@ -2,36 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:practice/immutable_widget.dart';
 
 class OneScreen extends StatelessWidget {
-
   const OneScreen({super.key});
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.indigo,
-      title: const Text('Practice web application'),
-      actions: const <Widget>[
-        Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Icon(Icons.edit),
-        )
-      ],
-    ),
-    body: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: const <Widget>[
-        ImmutableWidget(),
-      ],
-    ),
-    drawer: Drawer(
-      child: Container(
-        color: Colors.lightBlue,
-        child: const Center(
-          child: Text(
-            'This is sample text.'
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          title: const Text('Practice web application'),
+          actions: const <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Icon(Icons.edit),
+            )
+          ],
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const <Widget>[
+            ImmutableWidget(),
+          ],
+        ),
+        drawer: Drawer(
+          child: Container(
+            color: Colors.lightBlue,
+            child: const Center(
+              child: Text('This is sample text.'),
+            ),
           ),
         ),
-      ),
-    ),
-  );
+      );
 }
