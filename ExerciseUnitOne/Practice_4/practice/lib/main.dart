8fbe5339cc7175a71f23eee6e6b23c44c5fda3f9
paolomococci <paolomococci@gmail.com>
2023-01-26 07:32:49 +0100

    modified:   ExerciseUnitOne/Practice_4/practice/lib/main.dart

--- ExerciseUnitOne/Practice_4/practice/lib/main.dart
+++ ExerciseUnitOne/Practice_4/practice/lib/main.dart
@@ -4,9 +4,10 @@ import 'package:practice/one_screen.dart';
 void main() => runApp(const StaticApp());
 
 class StaticApp extends StatelessWidget {
-  
   const StaticApp({super.key});
 
   @override
-  Widget build(BuildContext context) => const MaterialApp(home: OneScreen(),);
+  Widget build(BuildContext context) => const MaterialApp(
+        home: OneScreen(),
+      );
 }
